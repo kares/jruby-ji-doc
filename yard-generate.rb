6c52d728d645cd3c5125327cc0f3d829dbce93c7
kares <self@kares.org>
2016-04-22 08:18:01 +0200

    yard generate .rb script (assuming to be living in a jruby source sub-dir)

---
+++ yard-generate.rb
@@ -0,0 +1,17 @@
+
+JRUBY_BASE_DIR = ENV['JRUBY_BASE_DIR'] || File.expand_path('..', File.dirname(__FILE__))
+
+RB_DOC_FILES = File.join('core/src/main/ruby/jruby/java/**/*.rb')
+
+require 'yard'
+
+argv = [ '--no-private', '--hide-void-return' ]
+
+argv.push '-o', File.expand_path(File.dirname(__FILE__)), RB_DOC_FILES
+
+Dir.chdir JRUBY_BASE_DIR do
+  YARD::CLI::CommandParser.run(*argv)
+end
+
+# yard -o ~/workspace/oss/jruby/._ji-doc/ ruby/jruby/java/**/*.rb
+
